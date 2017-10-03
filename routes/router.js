0751a1c6bf11db2d7f2d4a2bbf57c04176d1e7af
Cuong Nguyen <cuongnmd00335@gmail.com>
2017-10-03 10:47:41 +0700

    Router

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const index = require("./index-route");
-const user = require("./users-route");
-
 router.get("/", index);
-router.get("/", user);
 
 module.exports = router;
--- routes/users-route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/users", function(req, res, next) {
-    res.send("User");
-});
-
-module.exports = router;
