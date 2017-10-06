36a042c9980d8e56cb9ce948d1270ddcd509b3e8
huyenthu0712 <thudang84488@gmail.com>
2017-10-06 11:35:48 +0700

    Credential: convert role credential

--- http/middlewares/admin-require-middleware.js
+++ http/middlewares/admin-require-middleware.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-    // if (req.session.role !== "admin") {
-    //     res.redirect('login');
-    // }
+    if (req.session.role !== "admin") {
+        res.redirect('login');
+    }
 
     next();
 };
