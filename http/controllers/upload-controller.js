d43d512836f27bf1dc247bec537dbbadc4586533
huyboxi <dohuy171@gmail.com>
2017-11-01 10:45:33 +0700

    fix-error-image

--- http/controllers/upload-controller.js
+++ http/controllers/upload-controller.js
@@ -4,7 +4,7 @@ module.exports = function (req, res) {
 
     uploader.upload(req, res, function (err) {
         if (err) {
-            return res.status(500).json({link: '/uploads/default.jpg'});
+            return res.status(500).json({link: '/uploads/default.jpg', message: 'size of image not exceed 5mb'});
 
         }
         if (!req.file) {
